898d2c34675b65ab99f6aa4f499fb1de628c0172
Vish <vishpotnis@gmail.com>
2018-06-12 23:30:24 -0700

    adding train network

---
+++ train_network.py
@@ -0,0 +1,45 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('./data/driving_log.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	for line in reader:
+		lines.append(line)
+
+images = []
+measurements = []
+
+for line in lines:
+	source_path = line[0]
+	filename = source_path.split('/')[-1]
+	current_path = './data/IMG/' + filename
+	image = cv2.imread(current_path)
+	images.append(image)
+	measurement = float(line[3])
+	measurements.append(measurement)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+model.add(Convolution2D(32, 3, 3, input_shape=(160,320,3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+
+model.save('model.h5')
+
