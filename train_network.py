c9883fffb2b5f3d8cb249af73a9b238361284b72
Vish <vishpotnis@gmail.com>
2018-06-14 01:34:10 -0700

    train_network update

--- train_network.py
+++ train_network.py
@@ -1,45 +1,102 @@
 import csv
 import cv2
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Dropout, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from sklearn.model_selection import train_test_split
+import sklearn
+from random import shuffle
 
-lines = []
+samples = []
 with open('./data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
-		lines.append(line)
+		samples.append(line)
 
-images = []
-measurements = []
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-for line in lines:
-	source_path = line[0]
-	filename = source_path.split('/')[-1]
-	current_path = './data/IMG/' + filename
-	image = cv2.imread(current_path)
-	images.append(image)
-	measurement = float(line[3])
-	measurements.append(measurement)
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: 
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+            	for i in range(3):
+            		source_path = batch_sample[i]
+            		filename = source_path.split('/')[-1]
+            		current_path = './data/IMG/' + filename
+            		image = cv2.imread(current_path)
+            		angle = float(line[3])
+            		if i == 1:
+            			angle += 0.2
+            		elif i == 2:
+            			angle -= 0.2
+            		image_flipped = np.fliplr(image)
+            		angle_flipped = -angle
 
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Dropout
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
+            		images.append(image)
+            		angles.append(angle)
+            		images.append(image_flipped)
+            		angles.append(angle_flipped)
+            
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+
+# images = []
+# measurements = []
+
+# for line in lines:
+# 	for i in range(3):
+# 		source_path = line[0]	
+# 		filename = source_path.split('/')[-1]
+# 		current_path = './data/IMG/' + filename
+# 		image = cv2.imread(current_path)	
+# 		measurement = float(line[3])
+# 		if i == 1:
+# 			measurement += 0.2
+# 		elif i == 2:
+# 			measurement -= 0.2
+		
+# 		image_flipped = np.fliplr(image)
+# 		measurement_flipped = -measurement
+
+# 		images.append(image)	
+# 		measurements.append(measurement)
+# 		images.append(image_flipped)	
+# 		measurements.append(measurement_flipped)
+
+# X_train = np.array(images)
+# y_train = np.array(measurements)
+
 
 model = Sequential()
-model.add(Convolution2D(32, 3, 3, input_shape=(160,320,3)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.5))
-model.add(Activation('relu'))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+# model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5)
 
 model.save('model.h5')
 
